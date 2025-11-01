5a8beb097a03ca1b1c2fd02bcd514c5a7e94682f
Jordan Paris <hamologist.proton.dev>
2025-10-31 23:05:14 -0400

    Adds emojify slash command

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use poise::serenity_prelude as serenity;
-use dice_roll::{parser};
+use std::sync::Arc;
 
-struct Data {}
+use dice_roll::parser;
+use emojify::Emojifier;
+use poise::serenity_prelude as serenity;
+
+pub struct Data {
+    pub emojifier: Arc<Emojifier>,
+}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Replies with pong.
 #[poise::command(slash_command, prefix_command)]
-async fn ping(
-    ctx: Context<'_>,
-) -> Result<(), Error> {
+async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("Pong!".to_string()).await?;
     return Ok(());
 }
@@ -40,21 +43,31 @@ async fn roll(
     return Ok(());
 }
 
+/// Spice up a message with emojis.
+#[poise::command(slash_command, prefix_command)]
+async fn emojify(ctx: Context<'_>, message: String) -> Result<(), Error> {
+    ctx.reply(ctx.framework().user_data.emojifier.emojify(&message))
+        .await?;
+    return Ok(());
+}
+
 #[tokio::main]
 async fn main() {
     let token =
         std::env::var("DISCORD_TOKEN").expect("Missing environment variable: DISCORD_TOKEN");
     let intents = serenity::GatewayIntents::non_privileged();
 
+    let emojifier = Arc::new(Emojifier::new());
+
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![roll(), ping()],
+            commands: vec![roll(), ping(), emojify()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data { emojifier })
             })
         })
         .build();
